8fd150491468d9fd8d9d10c26d0aab394abcc72f
Tiffany Smalley <tiffanysmalley@berkeley.edu>
2023-11-01 19:37:59 -0700

    api call code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 //import { ThemeProvider } from '@aws-amplify/ui-react';
+import React, { useState } from 'react';
 import {
   MemeCardCollection,
   CaptionInput,
@@ -8,32 +9,48 @@ import {
   GeneratedMeme,
   InputBox,
   NavBarHeaderMemeHero,
-  MarketingFooter
+  MarketingFooter,
+  TemperatureSlider
 } from './ui-components';
 
 function App() {
+  const [caption, setCaption] = useState('');
+  const [temperature, setTemperature] = useState(0.5);
+  const [generatedImage, setGeneratedImage] = useState(null);
+
   const handleButtonClick = async() => {
     console.log('sending api call...');
     try {
       const response = await fetch('**api endpoint*', {
           method: 'POST', 
           headers: {
+            'Content-Type': 'application/json',
           },
-          body: JSON.stringify({"uuid": "test-uuid", "text_prompt": "i love aws", "temperature": 0.1})
+          body: JSON.stringify({"uuid": "test-uuid", "text_prompt": caption, "temperature": temperature})
       });
       const data = await response.json();
+      setGeneratedImage(data.imageUrl);
       console.log(data);
   } catch (error) {
       console.error('error making API call:', error);
   }
+};
 
+const handleCaptionChange = (event) => {
+  setCaption(event.target.value);
+};
+
+const handleTemperatureChange = (newValue) => {
+  setTemperature(newValue);
 };
 
   return (
     <div className="App">
-         <CaptionInput></CaptionInput>
-         <CallAPI></CallAPI>
+         <CaptionInput onChange={handleCaptionChange}></CaptionInput>
+         <CallAPI onClick={handleButtonClick}></CallAPI>
+         <TemperatureSlider onChange={handleTemperatureChange}></TemperatureSlider>
          <br />
+         {generatedImage && <GeneratedMeme imageUrl={generatedImage}></GeneratedMeme>}
          <br />
          <br />
       <NavBarHeaderMemeHero/>
